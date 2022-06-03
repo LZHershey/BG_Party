66d004ab01f64c4c38e8a3aed50bb78ff7fadc8e
LZHershey <laurenzhershey@gmail.com>
2022-06-03 15:18:09 -0700

    Models and seed created

--- script/seed.js
+++ script/seed.js
@@ -1,58 +1,156 @@
-'use strict'
+"use strict";
 
-const {db, models: {User} } = require('../server/db')
+const {
+  db,
+  models: { User, Game, Party },
+} = require("../server/db");
 
-/**
- * seed - this function clears the database, updates tables to
- *      match the models, and populates the database.
- */
-async function seed() {
-  await db.sync({ force: true }) // clears db and matches models to tables
-  console.log('db synced!')
+const users = [
+  {
+    username: "laurenzh",
+    password: "123",
+    displayName: "LZ",
+    durationPref: "30-60 min",
+    playPref: ["deck-building", "strategy", "party"],
+    complexityPref: "easy",
+  },
+  {
+    username: "dmakian",
+    password: "123",
+    displayName: "David",
+    durationPref: "3+ hrs",
+    playPref: ["drafting", "engine-building", "area-control"],
+    complexityPref: "complex",
+  },
+  {
+    username: "mred",
+    password: "123",
+    displayName: "Maria",
+    durationPref: "1-2 hrs",
+    playPref: ["campaign", "deck-building", "engine-building"],
+    complexityPref: "moderate",
+  },
+  {
+    username: "mikwa",
+    password: "123",
+    displayName: "Mikwa",
+    durationPref: "3+ hrs",
+    playPref: ["deck-building", "engine-building", "area-control"],
+    complexityPref: "complex",
+  },
+];
 
-  // Creating Users
-  const users = await Promise.all([
-    User.create({ username: 'cody', password: '123' }),
-    User.create({ username: 'murphy', password: '123' }),
-  ])
+const games = [
+  {
+    name: "Terraforming Mars",
+    minPlayers: 1,
+    maxPlayers: 5,
+    duration: "3+ hrs",
+    playStyle: ["engine-building", "area-control"],
+    complexity: "complex",
+  },
+  {
+    name: "Spirit Island",
+    minPlayers: 1,
+    maxPlayers: 4,
+    duration: "1-2 hrs",
+    playStyle: ["co-op", "strategy"],
+    complexity: "complex",
+  },
+  {
+    name: "Dumb Ass",
+    minPlayers: 2,
+    maxPlayers: 4,
+    duration: "30-60 min",
+    playStyle: ["party"],
+    complexity: "easy",
+  },
+  {
+    name: "Clank",
+    minPlayers: 2,
+    maxPlayers: 4,
+    duration: "30-60 min",
+    playStyle: ["deck-building", "strategy"],
+    complexity: "moderate",
+  },
+  {
+    name: "Wingspan",
+    minPlayers: 1,
+    maxPlayers: 5,
+    duration: "30-60 min",
+    playStyle: ["engine-building", "strategy"],
+    complexity: "moderate",
+  },
+  {
+    name: "Root",
+    minPlayers: 2,
+    maxPlayers: 4,
+    duration: "1-2 hrs",
+    playStyle: ["wargames", "strategy"],
+    complexity: "complex",
+  },
+];
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded successfully`)
-  return {
-    users: {
-      cody: users[0],
-      murphy: users[1]
-    }
-  }
-}
+const parties = [
+  {
+    name: "Friday Game Night",
+    location: "Mike & Maria's Apartment",
+    date: new Date("June 10, 2022 18:00"),
+  },
+  {
+    name: "Board Game Date Night",
+    location: "Lauren & David's Home",
+    date: new Date("June 12, 2022 19:00"),
+  },
+];
 
-/*
- We've separated the `seed` function from the `runSeed` function.
- This way we can isolate the error handling and exit trapping.
- The `seed` function is concerned only with modifying the database.
-*/
-async function runSeed() {
-  console.log('seeding...')
+const seed = async () => {
   try {
-    await seed()
+    await db.sync({ force: true });
+    const [lauren, david, maria, mike] = await Promise.all(
+      users.map((user) => {
+        return User.create(user);
+      })
+    );
+    const [terraforming, spiritIsland, dumbAss, clank, wingspan, root] =
+      await Promise.all(
+        games.map((game) => {
+          return Game.create(game);
+        })
+      );
+    const [fridayNight, dateNight] = await Promise.all(
+      parties.map((party) => {
+        return Party.create(party);
+      })
+    );
+
+    await lauren.addGames([terraforming, dumbAss]);
+    await david.addGames([spiritIsland]);
+    await maria.addGames([root, clank]);
+    await mike.addGames([wingspan]);
+    await fridayNight.addUsers([lauren, david, maria, mike]);
+    await dateNight.addUsers([lauren, david]);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function runSeed() {
+  console.log("seeding...");
+  try {
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log("closing db connection");
+    await db.close();
+    console.log("db connection closed");
   }
 }
 
-/*
-  Execute the `seed` function, IF we ran this module directly (`node seed`).
-  `Async` functions always return a promise, so we can use `catch` to handle
-  any errors that might occur inside of `seed`.
-*/
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
-// we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,18 @@
-//this is the access point for all things database related!
+const db = require("./db");
+const User = require("./models/User");
+const Game = require("./models/Game");
+const Party = require("./models/Party");
 
-const db = require('./db')
-
-const User = require('./models/User')
-
-//associations could go here!
+User.belongsToMany(Game, { through: "userGames" });
+Game.belongsToMany(User, { through: "userGames" });
+User.belongsToMany(Party, { through: "partyUsers" });
+Party.belongsToMany(User, { through: "partyUsers" });
 
 module.exports = {
   db,
   models: {
     User,
+    Game,
+    Party,
   },
-}
+};
